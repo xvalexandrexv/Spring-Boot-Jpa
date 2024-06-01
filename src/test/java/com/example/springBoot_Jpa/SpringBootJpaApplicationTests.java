ea9439f368eedf3d555c1fd28977b47c9d32670b
Alenxandre silva <xvalexandrexv@gmail.com>
2024-06-02 00:26:43 +0100

    project refractor

---
+++ src/test/java/com/example/springBoot_Jpa/SpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springBoot_Jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
